e2cc4f9afe36fb35f3f3efae5bc911d101de37ab
codemodel6@gmail.com <codemodel6@gmail.com>
2024-05-09 22:28:58 +0900

    rename: "최상위 폴더 이름 변경"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,8 @@ import Header from "./components/Organism/Header";
 import Home from "./page/home/Home";
 import Intro from "./page/intro/Intro";
 import Information from "./page/information/Map";
-import Project from "./page/project/Game";
-import Question from "./page/question/Board";
+import Project from "./page/game/Game";
+import Question from "./page/board/Board";
 import store from "./page/redux/store";
 
 function App() {
