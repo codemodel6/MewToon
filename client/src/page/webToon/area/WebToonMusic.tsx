b8e7d019342fb7093e6f8798a807bf6b58ff320f
codemodel6 <codemodel6@gmail.com>
2024-07-03 12:48:23 +0900

    feat : "WebToonModal 공통 함수 적용 완료"

--- client/src/page/webToon/area/WebToonMusic.tsx
+++ client/src/page/webToon/area/WebToonMusic.tsx
@@ -5,6 +5,11 @@ import play from "../../../components/CSS/image/MusicImg/play.png";
 import { useEffect, useRef, useState } from "react";
 import { musicArr } from "../../project/contents/musicBox/musicArr";
 import { MainColor } from "../../../components/CSS/Color/ColorNote";
+import {
+  handlePlay,
+  handleStop,
+  handleTimeChange,
+} from "../../../components/Function/music";
 
 const WebToonMusicList = styled.div`
   display: flex;
@@ -107,57 +112,6 @@ const WebToonMusic = () => {
     };
   }, []);
 
-  /** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  - 함수 기능 : 노래의 진행도를 표시해주는 함수
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  const handleChange = (value: string) => {
-    console.log("실행");
-    console.log("오이이", typeof value);
-
-    const newTime = parseFloat(value);
-    setCurrentTime(newTime);
-    if (tonnAudioRef.current) {
-      tonnAudioRef.current.currentTime = newTime;
-    }
-  };
-
-  /** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  - 함수 기능 : 노래 시작
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  const handlePlay = () => {
-    // state가 반영이 느린 문제점을 해결 : prevState는 변하기 전 상태 ex) true -> false면 true
-    setAudioState((prevState) => {
-      const nowState = true;
-      /** 함수 : setTimeout 0
-       *  비동기적으로 현재 실행중인 모든 코드가 실행되어서 호출 스택이 비워진 후
-       *  즉시 실행하도록 하는 함수이다. 이후 setPlayMusic이 완벽하게 동작한 후 음악을 실행시킨다.
-       *  함수를 나누지 않은 이유는 브라우저 정책에 의해 노래를 실행시키는 것은
-       *  오직 Click 이벤트 등에 의해 실행해야 하기 때문이다.
-       */
-      setTimeout(() => {
-        if (tonnAudioRef.current) {
-          tonnAudioRef.current.play(); // 노래 시작
-        }
-      }, 0);
-      return nowState;
-    });
-  };
-
-  /** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  - 함수 기능 : 노래 멈춤
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  const handleStop = () => {
-    // state가 반영이 느린 문제점을 해결
-    setAudioState((prevState) => {
-      // 현재 상태를 false 로 변경
-      const nowState = false;
-      if (tonnAudioRef.current) {
-        tonnAudioRef.current.pause(); // 노래 일시정지
-      }
-      return nowState;
-    });
-  };
-
   return (
     <WebToonMusicList>
       <audio ref={tonnAudioRef} src={playMusic} />
@@ -169,11 +123,19 @@ const WebToonMusic = () => {
           min="0"
           max={duration}
           step={0.1}
-          onChange={(e) => handleChange(e.target.value)}
+          onChange={(e) =>
+            handleTimeChange(e.target.value, setCurrentTime, tonnAudioRef)
+          }
         />
       </div>
       <div className="on-off-area">
-        <button onClick={audioState ? handleStop : handlePlay}>
+        <button
+          onClick={() =>
+            audioState
+              ? handleStop(setAudioState, tonnAudioRef)
+              : handlePlay(setAudioState, tonnAudioRef)
+          }
+        >
           <img src={audioState ? pause : play} alt="재생도구" />
         </button>
       </div>
