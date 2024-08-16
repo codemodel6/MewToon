c2db0d74b74f50a4390476a45599a351e865038c
codemodel6 <codemodel6@gmail.com>
2024-08-16 15:55:32 +0900

    feat : "kakaomap 도입"

--- client/src/page/information/area/Map.tsx
+++ client/src/page/information/area/Map.tsx
@@ -8,6 +8,8 @@ import {
   aroundRow,
   centerColumn,
 } from "../../../components/CSS/Global/GlobalDisplay";
+import { useEffect, useRef } from "react";
+import { GlobalButton } from "../../../components/CSS/Global/GlobalItem";
 
 const MapWrapper = styled.div`
   ${centerColumn}
@@ -23,16 +25,9 @@ const MapWrapper = styled.div`
 
   .mapToolDiv {
     ${aroundRow}
-    background-color: royalblue;
     width: 50%;
     height: 10%;
-
-    .mapMove {
-      width: 200px;
-      height: 50%;
-      background-color: white;
-      border: 1px solid ${MainColor.Main200};
-    }
+    border: 2px solid ${MainColor.Main100};
   }
 `;
 
@@ -40,32 +35,75 @@ declare global {
   interface Window {
     kakao: any;
   }
+  const kakao: any;
 }
 
 const Map = () => {
-  // useEffect(() => {
-  //  let mapContainer = document.getElementById("map"), // 지도를 표시할 div
-  //   mapOption = {
-  //    center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-  //    level: 3, // 지도의 확대 레벨
-  //   };
+  const mapRef = useRef<any | null>(null);
 
-  //  let map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+  useEffect(() => {
+    const mapContainer = document.getElementById("map"); // 지도를 표시할 div
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(
+        37.26645115502854,
+        127.00398815725785
+      ), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
 
-  //  const panTo = () => {
-  //   // 이동할 위도 경도 위치를 생성합니다
-  //   var moveLatLon = new window.kakao.maps.LatLng(33.45058, 126.574942);
+    // map 객체를 useRef에 저장
+    mapRef.current = new window.kakao.maps.Map(mapContainer, mapOption);
 
-  //   // 지도 중심을 부드럽게 이동시킵니다
-  //   // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-  //   map.panTo(moveLatLon);
-  //  };
-  // }, []);
+    // 마커를 표시할 위치와 title 객체 배열입니다
+    var positions = [
+      {
+        title: "본사",
+        latlng: new kakao.maps.LatLng(37.26645115502854, 127.00398815725785),
+      },
+      {
+        title: "지사",
+        latlng: new kakao.maps.LatLng(36.980948694022736, 126.9377708794878),
+      },
+    ];
+
+    // positions 배열을 순회하면서 마커를 생성하고 지도에 표시
+    positions.forEach((position) => {
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        position: position.latlng, // 마커를 표시할 위치
+        title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      });
+
+      // 마커를 지도에 표시합니다
+      marker.setMap(mapRef.current);
+    });
+  }, []);
+
+  const panTo = (latitude: number, longitude: number) => {
+    const moveLatLon = new window.kakao.maps.LatLng(latitude, longitude);
+
+    mapRef.current.panTo(moveLatLon);
+  };
 
   return (
     <MapWrapper>
       <div className="mapDiv" id="map"></div>
-      <div className="mapToolDiv"></div>
+      <div className="mapToolDiv">
+        <GlobalButton
+          width="30%"
+          height="50%"
+          onClick={() => panTo(37.26645115502854, 127.00398815725785)}
+        >
+          본사
+        </GlobalButton>
+        <GlobalButton
+          width="30%"
+          height="50%"
+          onClick={() => panTo(36.980948694022736, 126.9377708794878)}
+        >
+          지사
+        </GlobalButton>
+      </div>
     </MapWrapper>
   );
 };
